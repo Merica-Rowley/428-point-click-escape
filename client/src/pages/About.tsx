012d0009e1949b5f92ec2ed800b7503ddfd3ed46
Merica Rowley <mericarowley@gmail.com>
2025-10-13 16:01:44 -0600

    added content to About page

--- client/src/pages/About.tsx
+++ client/src/pages/About.tsx
@@ -4,7 +4,28 @@ export default function About() {
   return (
     <>
       <Navbar />
-      <h2>About Page</h2>;
+      <h2>About</h2>
+      <p>
+        Point Click Escape is a logic puzzle game. Players start out in a locked
+        room: why are they there, and how can they get out? It’s up to you to
+        find out! Explore the room, look for clues and tools, and find the key
+        to escape!
+      </p>
+      <h4>How to Play</h4>
+      <p>
+        In order to navigate, click on the arrows on the side, top, and bottom
+        of the screen. Click on areas of the room to take a closer look.
+      </p>
+      <p>
+        Each room contains various objects. When an object is collected, it will
+        appear in the inventory. To use an object, select the object from the
+        inventory and click on the desired area of use. Once an object has been
+        used up, it will disappear from the inventory.
+      </p>
+      <p>
+        If you’d like to take a break and come back later, don’t forget to save
+        your game!
+      </p>
     </>
   );
 }
