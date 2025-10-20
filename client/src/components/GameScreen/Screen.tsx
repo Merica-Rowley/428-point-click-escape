62db035f9de4d53153726d911b82f9849ef354d9
JonahThurston <jonah.m.thurston@gmail.com>
2025-10-20 17:01:47 -0600

    finish up inventory system

--- client/src/components/GameScreen/Screen.tsx
+++ client/src/components/GameScreen/Screen.tsx
@@ -1,21 +1,38 @@
 import { useState } from "react";
 import "./Screen.css";
 import key from "../../assets/Screen1/key.png";
+import door from "../../assets/Screen1/door.png";
+import type { item } from "../../pages/Game/Game";
 
 export default function GameScreen(props: {
-  inventory: { key: boolean };
+  inventory: item[];
   onPickUpItem: (itemName: string) => void;
+  selectedItem: string;
 }) {
   const [bgIndex, setBgIndex] = useState(0);
   const backgrounds = ["bg-one", "bg-two"];
 
+  const [showWin, setShowWin] = useState(false);
+
+  const handleDoorClick = () => {
+    if (props.selectedItem === "key") {
+      setShowWin(true);
+    }
+  };
+
   return (
     <div className={`background-container ${backgrounds[bgIndex]}`}>
-      {!props.inventory.key && (
+      {!props.inventory.find((i) => i.name === "key") && (
         <button className="keyButton" onClick={() => props.onPickUpItem("key")}>
           <img src={key} alt="key" />
         </button>
       )}
+
+      <button className="doorButton" onClick={() => handleDoorClick()}>
+        <img src={door} alt="door" />
+      </button>
+
+      {showWin && <h1>YOU WIN</h1>}
     </div>
   );
 }
--- client/src/components/Inventory/Inventory.tsx
+++ client/src/components/Inventory/Inventory.tsx
@@ -1,3 +1,24 @@
-export default function Inventory() {
-  return <p>Inventory component works</p>;
+import type { item } from "../../pages/Game/Game";
+import key from "../../assets/Screen1/key.png";
+
+export default function Inventory(props: {
+  inventory: item[];
+  selectItem: (item: string) => void;
+}) {
+  const displayItems = props.inventory.slice(0, 5);
+  return (
+    <div className="inventory-bar">
+      {displayItems.length > 0 &&
+        displayItems.map((item, index) => (
+          <div className="inventory-box" key={index}>
+            <button
+              className="keyButton"
+              onClick={() => props.selectItem("key")}
+            >
+              <img src={key} alt="key" />
+            </button>
+          </div>
+        ))}
+    </div>
+  );
 }
