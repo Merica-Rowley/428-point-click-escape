1038678bdc54566cc196807eb0772b3906d7c5a1
JonahThurston <jonah.m.thurston@gmail.com>
2025-10-22 14:58:56 -0600

    Frontend main functionality done

--- client/src/components/Inventory/Inventory.tsx
+++ client/src/components/Inventory/Inventory.tsx
@@ -1,10 +1,19 @@
 import type { item } from "../../pages/Game/Game";
 import key from "../../assets/Screen1/key.png";
+import { useState } from "react";
+import "./Inventory.css";
 
 export default function Inventory(props: {
   inventory: item[];
   selectItem: (item: string) => void;
 }) {
+  const [selectedIndex, setSelectedIndex] = useState<number>(-1);
+
+  const inventorySelect = (itemName: string, index: number) => {
+    props.selectItem(itemName);
+    setSelectedIndex(index);
+  };
+
   const displayItems = props.inventory.slice(0, 5);
   return (
     <div className="inventory-bar">
@@ -12,10 +21,12 @@ export default function Inventory(props: {
         displayItems.map((item, index) => (
           <div className="inventory-box" key={index}>
             <button
-              className="keyButton"
-              onClick={() => props.selectItem("key")}
+              className={`${item.name}-button ${
+                index === selectedIndex ? "selected-item" : ""
+              }`}
+              onClick={() => inventorySelect(item.name, index)}
             >
-              <img src={key} alt="key" />
+              <img src={key} alt="item.name" />
             </button>
           </div>
         ))}
