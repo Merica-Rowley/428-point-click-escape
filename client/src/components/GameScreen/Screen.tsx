46ea966c556d72db54336a9b0d117b20b82fa46d
JonahThurston <jonah.m.thurston@gmail.com>
2025-10-31 14:20:21 -0600

    Dynamic background pages

--- client/src/components/GameScreen/Screen.tsx
+++ client/src/components/GameScreen/Screen.tsx
@@ -2,18 +2,25 @@ import { useState } from "react";
 import "./Screen.css";
 import key from "../../assets/Screen1/key.png";
 import door from "../../assets/Screen1/door.png";
-import type { item } from "../../pages/Game/Game";
+import type { item, worldFlag } from "../../pages/Game/Game";
 
 export default function GameScreen(props: {
   inventory: item[];
+  worldState: worldFlag[];
   onPickUpItem: (itemName: string) => void;
   selectedItem: string;
+  toggleWorldFlag: (flagName: string) => void;
 }) {
   const [showWin, setShowWin] = useState(false);
 
   const [screenIndex, setScreenIndex] = useState(0);
 
+  const checkWorldFlag = (flagName: string) => {
+    return props.worldState.some((f) => f.name === flagName && f.state);
+  };
+
   const handleDoorClick = () => {
+    props.toggleWorldFlag("victory");
     if (props.selectedItem === "key") {
       setShowWin(true);
     }
@@ -26,7 +33,13 @@ export default function GameScreen(props: {
   switch (screenIndex) {
     case 0:
       return (
-        <div className="background-container bg-one">
+        <div
+          className={`background-container ${
+            checkWorldFlag("victory")
+              ? "screen-one-bg-two"
+              : "screen-one-bg-one"
+          }`}
+        >
           {!props.inventory.find((i) => i.name === "key") && (
             <button
               className="keyButton"
