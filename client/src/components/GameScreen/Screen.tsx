d9b8d60e50eea29202c6231d8548fc9067fb048b
JonahThurston <jonah.m.thurston@gmail.com>
2025-11-07 14:57:17 -0700

    implement desk screen

--- client/src/components/GameScreen/Screen.tsx
+++ client/src/components/GameScreen/Screen.tsx
@@ -3,6 +3,7 @@ import "./Screen.css";
 import key from "../../assets/Screen1/key.png";
 import door from "../../assets/Screen1/door.png";
 import screwdriver from "../../assets/Screen3/screwdriver.png";
+import closedBook from "../../assets/Screen6/closed-book.png";
 import type { item, worldFlag } from "../../pages/Game/Game";
 
 export default function GameScreen(props: {
@@ -41,6 +42,10 @@ export default function GameScreen(props: {
       return;
     }
     setScreenIndex(screenIndex - 1);
+  };
+
+  const handleGoDesk = () => {
+    setScreenIndex(6);
   };
 
   switch (screenIndex) {
@@ -117,6 +122,41 @@ export default function GameScreen(props: {
           <button className="goLeft" onClick={() => handleGoLeft()}>
             LEFT ARROW<br></br>PLACEHOLDER
           </button>
+          <button className="desk" onClick={() => handleGoDesk()}></button>
+        </div>
+      );
+
+    case 6:
+      //TODO: still need bg assets for this one
+      return (
+        <div className={`background-container screen-six-bg-one`}>
+          {checkWorldFlag("openedBook") ? (
+            <div className="opened-book">K4t4r4W4t3rB3nd3r!</div>
+          ) : (
+            <button
+              className="book"
+              onClick={() => props.toggleWorldFlag("openedBook")}
+            >
+              <img src={closedBook} alt="book" />
+            </button>
+          )}
+          {checkWorldFlag("openedDrawer") ? (
+            <div className="opened-drawer">
+              {!props.inventory.find((i) => i.name === "drawer-button") && (
+                <button
+                  className="drawer-button"
+                  onClick={() => props.onPickUpItem("drawer-button")}
+                >
+                  ButtonPlaceholder
+                </button>
+              )}
+            </div>
+          ) : (
+            <button
+              className="drawer-knob"
+              onClick={() => props.toggleWorldFlag("openedDrawer")}
+            ></button>
+          )}
         </div>
       );
     default:
