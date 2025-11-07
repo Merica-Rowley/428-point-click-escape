7f986099ba1fc43d9cb7df44e1c6b905a9d0b0cb
EMoreno65 <ethanmichaelmoreno@gmail.com>
2025-11-07 14:27:02 -0700

    Login and Register now work

--- client/src/components/navbar/Navbar.tsx
+++ client/src/components/navbar/Navbar.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { useAuth } from "../auth/AuthHelper";
+import { useAuth } from "../auth/AuthProvider";
 import "./Navbar.css";
 
 export default function Navbar() {
--- server/server.ts
+++ server/server.ts
@@ -16,6 +16,8 @@ const pool = new Pool({
   },
 });
 
+// One small edit to test DB connection
+
 pool.query("SELECT NOW()")
   .then(res => console.log("DB connected:", res.rows[0]))
   .catch(err => console.error("DB connection failed:", err));
@@ -42,6 +44,12 @@ app.post("/auth/register", async (req: Request, res: Response) => {
     const result = await pool.query(
       "INSERT INTO auth_table (username) VALUES ($1) RETURNING *",
       [name]
+    );
+
+    // Insert save state file
+    const world_result = await pool.query(
+      "INSERT INTO save_state (username, inventory, room, world_state) VALUES ($1, $2, $3, $4)",
+      [name, [], 1, [["Button in Drawer", false], ["Trap Door Visible", false], ["Password Entered", false], ["Trap Door Button Present", false], ["Painting Fallen", false], ["Screwdriver in Tank", false], ["Unscrewed Safe", false], ["First Button on Safe", false], ["Second Button on Safe", false], ["Third Button on Safe", false], ["Key in Safe", false], ["Lightbulb Down", false], ["Lightbulb Clicked", false], ["Lightbulb in Lamp", false], ["Panel on Wall Opened", false], ["Lamp on", false], ["Button in Panel Clicked", false], ["Code in Thermo 1", false], ["Code in Thermo 2", false], ["Code in Thermo 3", false]]]
     );
 
     res.json(result.rows[0]);
@@ -87,6 +95,37 @@ app.get("/game/inventory", async (req: Request, res: Response) => {
   }
 });
 
+app.post("/game/inventory", async (req: Request, res: Response) => {
+  const { name, item } = req.body;
+  console.log("Received request body:", req.body);
+
+  try {
+    const result = await pool.query(
+      `
+      UPDATE save_state
+      SET inventory = array_append(COALESCE(inventory, '{}'), $1)
+      WHERE username = $2
+      RETURNING inventory
+      `,
+      [item, name]
+    );
+
+
+    console.log("Query result:", result.rows); // <--- add this
+    console.log("Row count:", result.rowCount); // <--- add this
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    res.json(result.rows[0]);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Database error" });
+  }
+});
+
+
 app.get("/game/room", async (req: Request, res: Response) => {
   const { name } = req.body;
 
@@ -110,6 +149,31 @@ app.get("/game/state", async (req: Request, res: Response) => {
     const result = await pool.query("SELECT world_state FROM save_state WHERE username = $1", [name]);
     if (result.rows.length === 0) {
       return res.status(404).json({ error: "World State for given user not found" });
+    }
+
+    res.json(result.rows[0]);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Database error" });
+  }
+});
+
+app.post("/game/state", async (req: Request, res: Response) => {
+  const { name, world_state } = req.body;
+
+  try {
+    const result = await pool.query(
+      `
+      UPDATE save_state
+      SET world_state = $1
+      WHERE username = $2
+      RETURNING world_state
+      `,
+      [world_state, name]
+    );
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({ error: "User not found" });
     }
 
     res.json(result.rows[0]);
